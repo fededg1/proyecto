0ac8dd9d3c4562cbcef5e02a138d334755bc452a
fededg1 <96213588+fededg1@users.noreply.github.com>
2022-01-05 10:55:18 -0300

    Add files via upload

---
+++ dni.js
@@ -0,0 +1,11 @@
+var getData = function (){
+var DNI = document.getElementById("DNI").value;
+console.log (DNI);
+
+const ROLAPI = 
+if (DNI) {
+    
+} else { print "No se ha encontrado la persona"
+    
+}
+}
