1c197a5fe479e9c3ade4c55c541a920bc92279e8
fededg1 <96213588+fededg1@users.noreply.github.com>
2022-01-18 13:16:23 -0300

    Delete header.php

--- header.php
+++
@@ -1,9 +0,0 @@
-<header class='header'>
-    <nav class='botones principales'>
-        <ul>
-            <li> <a href="index.php"> Home </a> </li>
-            <li> <a href="contacto.php"> Contacto </a> </li>
-            <li> <a href="inscripcion.php"> Inscribirse </a> </li>
-        </ul>
-    </nav>
-</header>
